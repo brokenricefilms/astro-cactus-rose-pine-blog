873f2dfc9a48aac1bb7779d73f4217ec28d41bc8
Thuan Pham <thuanowa@gmail.com>
2023-01-31 18:57:35 +0700

    ref: rose pine

--- src/site.config.ts
+++ src/site.config.ts
@@ -1,11 +1,11 @@
 const siteMeta = {
 	title: "thuanowa",
-	description: "Thuan Pham aka thuanowa portfolio website",
+	description: "Thuan Pham aka thuanowa personal website",
 	lang: "en-US",
 	ogLocale: "en_US",
 	githubUrl: "https://github.com/thuanowa",
-	themeColorLight: "#fafafa",
-	themeColorDark: "#1d1f21",
+	themeColorLight: "#faf4ed",
+	themeColorDark: "#191724",
 };
 
 export default siteMeta;
