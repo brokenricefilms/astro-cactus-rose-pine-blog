b0fba40353a35d934f5eacca298ea03385d35f26
Thuan Pham <thuanowa@gmail.com>
2023-02-05 20:22:02 +0700

    chore: post

--- .astro/types.d.ts
+++ .astro/types.d.ts
@@ -112,6 +112,13 @@ declare module 'astro:content' {
   collection: "post",
   data: InferEntrySchema<"post">
 },
+"daily-journal-feb-5-2023.md": {
+  id: "daily-journal-feb-5-2023.md",
+  slug: "daily-journal-feb-5-2023",
+  body: string,
+  collection: "post",
+  data: InferEntrySchema<"post">
+},
 "github-co-gi-vui.md": {
   id: "github-co-gi-vui.md",
   slug: "github-co-gi-vui",
