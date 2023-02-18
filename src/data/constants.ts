e7ba78bb6baf2bb2ef7fd544916d2763273e7bbe
Thuan Pham <thuanowa@gmail.com>
2023-02-18 11:53:07 +0700

    chore(astro theme cactus): v2.0.3

---
+++ src/data/constants.ts
@@ -0,0 +1,14 @@
+export const MENU_LINKS = [
+	{
+		title: "Projects",
+		path: "/posts/projects",
+	},
+	{
+		title: "Writing",
+		path: "/posts",
+	},
+	{
+		title: "What I use?",
+		path: "/posts/what-i-use",
+	},
+];
