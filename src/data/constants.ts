d1bbff376ae1b4b6096b80a9d14f9cde3528502b
Thuan Pham <thuanowa@gmail.com>
2023-02-23 19:51:39 +0700

    chore(astro-theme-cactus): v2.1.0

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -12,3 +12,11 @@ export const MENU_LINKS = [
 		path: "/posts/what-i-use",
 	},
 ];
+
+export const SOCIAL_LINKS = {
+	github: "https://github.com/thuanowa",
+	discord: "https://discord.com/channels/@me/982102366255988776",
+	youtube: "https://youtube.com/@thuanowa",
+	figma: "https://figma.com/@thuanowa",
+	email: "thuanowa@gmail.com",
+};
