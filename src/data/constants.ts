b312e4885cf3cd2bc07848c6e45434310a55df50
Thuan Pham <thuanowa@gmail.com>
2023-03-30 21:24:38 +0700

    ref: the garden of thoughs

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -1,14 +1,22 @@
 export const MENU_LINKS = [
 	{
-		title: "Writing",
-		path: "/posts",
+		title: 'Home',
+		path: '/',
+	},
+	{
+		title: 'Writing',
+		path: '/posts',
+	},
+	{
+		title: 'The garden of thoughts',
+		path: 'https://github.com/thuanowa/garden/blob/main/The%20garden%20of%20thoughts.md#the-garden-of-thoughts',
 	},
 ];
 
 export const SOCIAL_LINKS = {
-	github: "https://github.com/thuanowa",
-	discord: "https://discord.com/channels/@me/982102366255988776",
-	youtube: "https://youtube.com/@thuanowa",
-	figma: "https://figma.com/@thuanowa",
-	email: "thuanowa@gmail.com",
+	github: 'https://github.com/thuanowa',
+	discord: 'https://discord.com/channels/@me/982102366255988776',
+	youtube: 'https://youtube.com/@thuanowa',
+	figma: 'https://figma.com/@thuanowa',
+	email: 'thuanowa@gmail.com',
 };
